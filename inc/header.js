3b10635cfd0b548463bfbbdf49177d96e1a4e907
Julio Alvarado <julioalvarado1019@gmail.com>
2016-06-15 12:37:52 -0500

    Agregando codigo para ocultar apartados de modificar y eliminar de cada tabla ( faltan los de reseñas y costos ), asi como para centrar el apartado de agregar

--- inc/header.js
+++ inc/header.js
@@ -4,7 +4,7 @@
 */
 
 $('document').ready(function(e){
-	Materialize.toast("Listo el header", 9000);
+	Materialize.toast("Listo el header", 4000);
 	console.log('Metodo de header.js leerCookie(nivel) : ' + leerCookie('nivel'));
 
 	var cookie = leerCookie('nivel').substring(0, 1);
@@ -14,6 +14,58 @@ $('document').ready(function(e){
 	{
 		$("#modificar_clientes").hide();
 		$("#eliminar_clientes").hide();
+		$("#row1_clientes").prepend("<div class='col s12 l2 white-text'>AA</div>");
+		$("#agregar_clientes").removeClass("l4");
+		$("#agregar_clientes").addClass("l8 offset-l2");
+
+        $("#modificar_autoridades").hide();
+		$("#eliminar_autoridades").hide();
+		$("#row1_autoridades").prepend("<div class='col s12 l2 white-text'>AA</div>");
+		$("#agregar_autoridades").removeClass("l4");
+		$("#agregar_autoridades").addClass("l8 offset-l2");
+
+		$("#modificar_gruas").hide();
+		$("#eliminar_gruas").hide();
+		$("#row1_gruas").prepend("<div class='col s12 l2 white-text'>AA</div>");
+		$("#agregar_gruas").removeClass("l4");
+		$("#agregar_gruas").addClass("l8 offset-l2");
+
+		$("#modificar_vehiculos").hide();
+		$("#eliminar_vehiculos").hide();
+		$("#row1_vehiculos").prepend("<div class='col s12 l2 white-text'>AA</div>");
+		$("#agregar_vehiculos").removeClass("l4");
+		$("#agregar_vehiculos").addClass("l8 offset-l2");
+
+		$("#modificar_remolques").hide();
+		$("#eliminar_remolques").hide();
+		$("#row1_remolques").prepend("<div class='col s12 l2 white-text'>AA</div>");
+		$("#agregar_remolques").removeClass("l4");
+		$("#agregar_remolques").addClass("l8 offset-l2");
+
+		$("#modificar_op").hide();
+		$("#eliminar_op").hide();
+		$("#row1_op").prepend("<div class='col s12 l2 white-text'>AA</div>");
+		$("#agregar_op").removeClass("l4");
+		$("#agregar_op").addClass("l8 offset-l2");
+
+		$("#modificar_re").hide();
+		$("#eliminar_re").hide();
+		$("#row1_re").prepend("<div class='col s12 l2 white-text'>AA</div>");
+		$("#agregar_re").removeClass("l4");
+		$("#agregar_re").addClass("l8 offset-l2");
+
+		$("#modificar_rv").hide();
+		$("#eliminar_rv").hide();
+		$("#row1_rv").prepend("<div class='col s12 l2 white-text'>AA</div>");
+		$("#agregar_rv").removeClass("l4");
+		$("#agregar_rv").addClass("l8 offset-l2");
+
+		$("#modificar_memoria").hide();
+		$("#eliminar_memoria").hide();
+		$("#row1_memoria").prepend("<div class='col s12 l2 white-text'>AA</div>");
+		$("#agregar_memoria").removeClass("l4");
+		$("#agregar_memoria").addClass("l8 offset-l2");
+
 	}
 
 /*
