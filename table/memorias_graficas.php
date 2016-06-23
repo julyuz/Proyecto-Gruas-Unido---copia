211f0fa1a344dbb8112a9b5ee48447321c68f97a
Julio Alvarado <julioalvarado1019@gmail.com>
2016-06-23 02:52:25 -0500

    Agregando metodos ( SQL ) para obtener distintos datos de tablas, agregando caso de 'get_img_perMG' para tener las imagenes a agregar al PDF, agregando codigo necesario para modificar la MG, en la tabla 'fotos' se eliminan los registros anteriores de esa MG y se insertan los nuevos, en la tabla de MG, se actualizan normalmente ( con UPDATE ), agregando codigo para generar un PDF con las imagenes obtenidas de acuerdo a la MG seleccionada ( en proceso )

--- table/memorias_graficas.php
+++ table/memorias_graficas.php
@@ -1,4 +1,6 @@
 <?php
+ob_end_clean();
+ini_set ('error_reporting', E_ALL);
 include('conexion.php');
 //include 'funciones_generales.php';
 
@@ -22,9 +24,9 @@ switch ($metodo) {
 
     break;
 
-  case 'create_pdf_MG':
-    # code...
-    require('../fpdf181/fpdf.php');
+  case 'create_pdf': // Crear pdf solo texto
+      # code...
+      require('../fpdf181/fpdf.php');
 
     class PDF extends FPDF
     {
@@ -100,31 +102,139 @@ TEL. CEL. 4525257450'), 0, 'J');
     $pdf->SetFont('Times','',12);
     $pdf -> Ln(30);
 
+    //for($i=1;$i<=40;$i++)
+      //  $pdf->Cell(0,10,'Imprimiendo línea número '.$i,0,1);
+    $pdf->Output('I');
+
+    //$fichero = 'Memoria_grafica.pdf';
+    //$pdf->Output($fichero,'D');
+
+      break;
+
+  case 'create_pdf_MG': // Crear pdf con las imagenes de la memoria_grafica
+    # code...
+    require('../fpdf181/fpdf.php');
+
+    class PDF extends FPDF
+    {
+        /*
+            ** Medidas de un PDF **
+            Anchura : 612 px.
+            Altura : 792 px.
+            Resol.: 72 dpi
+        */
+
+        // Cabecera de página
+        function Header()
+        {
+            $x = 190; $y = 0; $salto = 5; $margen_izq = 5;
+
+            // Logo izquierda ( águila )
+            $this->Image('../img/LOGO CLASICO29052015_0000.jpg',$margen_izq,3,35);
+
+            // Titulo
+            /*$this->SetFont('Arial', 'B', 10);
+            $this->SetXY(50, 5);
+            $this-> Multicell($x - 80, 5, utf8_decode('GRUAS MANCERA
+BOULEVARD INDUSTRIAL NO. 5700
+COL. CALZONTZIN (ENFRENTE DE PEMEX)
+TELS(452)5281045, 5249196 Correo: gmancerah@hotmail.com
+URUAPAN, MICHOACAN'), 1, 'C');*/
+
+          // Titulo
+            $this->SetFont('Arial','B',18); // Arial bold 18
+            $this->Cell($x,$y,'GRUAS MANCERA',0,0,'C');
+            $this->Ln(7); // Salto de línea
+            $this->SetFont('Arial','B',10); // Arial bold 10
+            $this->Cell($x,$y,'BOULEVARD INDUSTRIAL NO. 5700',0,0,'C');
+            $this -> Ln($salto); // Salto de línea
+            $this -> Cell($x,$y, 'COL. CALZONTZIN  (ENFRENTE DE PEMEX)', 0, 0, 'C');
+            $this -> Ln($salto); // Salto de línea
+            $this -> Cell($x, $y, 'TELS(452)5281045, 5249196 Correo: gmancerah@hotmail.com    ', 0, 0, 'C');
+            $this -> Ln($salto); // Salto de línea
+            $this -> Cell($x, $y, 'URUAPAN, MICHOACAN', 0, 0,'C');
+
+          // Logo derecha ( remolque )
+            $this ->Image('../img/LOGO PLATAFORMA29052015_0000.jpg', ($x - 30), 3, 45);
+
+        }
+
+        // Pie de página
+        function Footer()
+        {
+
+            $this -> SetFont('Arial', '', 10);
+            $this -> SetY(-55);
+            $this -> MultiCell(100, 5, utf8_decode('ELABORO:
+
+
+
+ING. GERARDO ALFONSO MANCERA HUANTE
+REPRESENTANTE DE GRUAS MANCERA
+TEL. CEL. 4525257450'), 0, 'J');
+
+            // Posición: a 1,5 cm del final
+            $this->SetY(-15);
+            // Arial italic 8
+            $this->SetFont('Arial','',8);
+            // Número de página
+            $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+        }
+    } // Fin class PDF
+
+    // Creación del objeto de la clase heredada
+    $pdf = new PDF();
+    $pdf->AliasNbPages();
+    $pdf->AddPage();
+    $pdf->SetFont('Times','',12);
+    $pdf -> Ln(10);
+
+
+        /*
+            ** Medidas de un PDF **
+            Anchura : 612 px.
+            Altura : 792 px.
+            Resol.: 72 dpi
+        */
+
+  /*  // Aqui van los datos del cliente
+    $pdf->SetFont('Arial','',12); // Arial bold 18
+            $pdf->Cell(0, 5,'DATOS DEL CLIENTE',0,0,'L');
+            $pdf->Ln(7); // Salto de línea
+*/
 
     // Creamos variables 'x' e 'y' para ubicar las imagenes
-    /*$xImg = 100; $yImg = 100; $anImg = 50; $alImg = 50;
-    // Recibimos el array 'data' con las imagenes subidas, para ubicarlas en el pdf que se va a crear
-    if( isset( $_POST["data"] ))
+    $xImg = 30; $yImg = 70; $anImg = 150; $alImg = 70;
+    //$pdf -> Image('../img/nemo.jpg', $xImg, $yImg, $anImg);
+
+    /*// Recibimos el array 'data' con las imagenes de la MG seleccionada, para ubicarlas en el pdf que se va a crear
+    if( isset( $_POST["data"] ) )
     {
-      $data = json_decode( stripslashes( $_POST["data"] ) );
+      $data = explode(",", $_POST["data"]);
 
       for ($i = 0; $i < count($data); $i++)
       {
-          $pdf ->Image('../img/' .$data[$i], $xImg, $yImg, $anImg);
-          $yImg = $yImg + 120;
-
+          $pdf ->Image('../img/subidas/' .$data[$i], $xImg, $yImg, $anImg, $alImg);
+          $yImg = $yImg + 90;
+          //if( $yImg )
+          //echo "\n*** Dentro del for ***\n data[ " .$i ." ] --> " .$data[$i] ." length: " . strlen($data[$i]);
       }
     }
     else {
       die("Datos de imagenes no validos");
-    }*/
+    }
 
-    //for($i=1;$i<=40;$i++)
-      //  $pdf->Cell(0,10,'Imprimiendo línea número '.$i,0,1);
-    //$pdf->Output('I');
+    //$pdf -> Output();
+*/
 
-    $fichero = 'Memoria_grafica.pdf';
-    $pdf->Output($fichero,'D');
+    for($i=1;$i<=40;$i++)
+        $pdf->Cell(0,10,'Imprimiendo línea número '.$i,0,1);
+    $pdf->Output('I');
+
+    //$fichero = 'Memoria_grafica.pdf';
+    //$pdf->Output($fichero,'D');
+
+
     /*$pdf2 = 'archivo.pdf';
     header('Content-type: application/pdf');
     //header('Content-Disposition: attachment; filename="'.$pdf2.'"');
@@ -146,7 +256,7 @@ TEL. CEL. 4525257450'), 0, 'J');
 
     break;
 
-    case 'get_photos_perMG':
+    case 'get_photos_perMG': // Este metodo ( no es general ) solo sirve si se han seleccionado imagenes en el input
       # code...
       header("Access-Control-Allow-Origin: *");
 
@@ -159,15 +269,16 @@ TEL. CEL. 4525257450'), 0, 'J');
           //if ($error == UPLOAD_ERR_OK)
           //{
               $nombre_tmp = $_FILES["imagenes_add"]["tmp_name"][$clave];
-              $nombre .= $_FILES["imagenes_add"]["name"][$clave];
+              $nombre .= $_FILES["imagenes_add"]["name"][$clave] .= ",";
               //move_uploaded_file($nombre_tmp,
                 //"../js/img/subidas/$nombre");
 
               $data[]= $_FILES["imagenes_add"]["name"][$clave];
       }
+        //print_r($data);
+        echo $nombre;
 
         //echo json_encode($data);
-        print_r($data);
           //}
         //echo "nombres: " .$nombre;
 
@@ -387,7 +498,7 @@ TEL. CEL. 4525257450'), 0, 'J');
           $extension = end($temp);
           //echo "img_name: " . $_FILES['imagenes_add']['name'][$clave] ." temp: " .$temp[1];
           $imagen="";
-          $random=rand(1,999999);
+          $random=rand(1,9999999);
 
             if ((($_FILES["imagenes_add"]["type"][$clave] == "image/gif")
               || ($_FILES["imagenes_add"]["type"][$clave] == "image/jpeg")
@@ -408,8 +519,8 @@ TEL. CEL. 4525257450'), 0, 'J');
                   }
                   else{
                       //move_uploaded_file($_FILES["imagenes_add"]["tmp_name"],
-                        //"../img/subidas/" .$random.'_'.$_FILES["imagenes_add"]["name"]);
-                        //echo "Archivo guardado en " . "../img/subidas/" .$random.'_'. $_FILES["imagenes_add"]["name"];
+                        //"../img/subida.'_'.$_FILES["imagenes_add"]["name"]);
+                        //echo "Archivo guardado en " . "../img/subida.'_'. $_FILES["imagenes_add"]["name"];
 
                       $temporal=$_FILES['imagenes_add']['tmp_name'][$clave];
                     //$nombr=$_FILES['file']['name'];
@@ -452,8 +563,7 @@ TEL. CEL. 4525257450'), 0, 'J');
 
                         $conn->query("INSERT into fotos(placas, ruta_img, thumbnail, id_vehiculo,id_memoria_grafica)
                           values ('$placas','$img','$thumbnail', '$id_vehiculo', '$id_MG')");
-                        echo true;
-
+                        //echo true; // Esto regresa un 1 ( uno )
 
                         imagedestroy($original);
                         imagedestroy($copia);
@@ -479,10 +589,21 @@ TEL. CEL. 4525257450'), 0, 'J');
     {
       $nombre_img = "imagenes_mod";
       $placas = $_POST["placas"];
+
+      $id_MG = $_POST["id_MG"];
+
+      $conn->query("DELETE from fotos WHERE id_memoria_grafica ='$id_MG'");
+
+        // Checar si se ejecuto correctamente la sentencia, si es mayor a 0, se ejecuto correctamente
+        if( $conn -> affected_rows > 0 )
+           echo " BORRADAS affected_rows: " . $conn -> affected_rows ." ";
+         else
+           echo false;
+
       foreach ($_FILES[$nombre_img]["error"] as $clave => $error)
       {
           $allowedExts = array("gif", "jpeg", "jpg", "png");
-          $temp = explode(".", $_FILES[$nombre_img]["name"][$clave]);
+          $temp = explode(".", $_FILES[$nombre_img]["name"][$clave]); // Es como split en Java
           $extension = end($temp);
           //echo "img_name: " . $_FILES['imagenes_add']['name'][$clave] ." temp: " .$temp[1];
           $imagen="";
@@ -507,8 +628,8 @@ TEL. CEL. 4525257450'), 0, 'J');
                   }
                   else{
                       //move_uploaded_file($_FILES["imagenes_add"]["tmp_name"],
-                        //"../img/subidas/" .$random.'_'.$_FILES["imagenes_add"]["name"]);
-                        //echo "Archivo guardado en " . "../img/subidas/" .$random.'_'. $_FILES["imagenes_add"]["name"];
+                        //"../img/subida.'_'.$_FILES["imagenes_add"]["name"]);
+                        //echo "Archivo guardado en " . "../img/subida.'_'. $_FILES["imagenes_add"]["name"];
 
                       $temporal=$_FILES[$nombre_img]['tmp_name'][$clave];
                     //$nombr=$_FILES['file']['name'];
@@ -531,9 +652,7 @@ TEL. CEL. 4525257450'), 0, 'J');
                     imagecopyresampled($copia, $original, 0,0,0,0, $ancho_nuevo,$alto_nuevo, $ancho_original, $alto_original);
                     imagejpeg($copia,"../img/subidas/".$imagen,100);
 
-                        echo "Placas: " .$placas;
-
-                        $id_MG = $_POST["id_MG"];
+                        echo " Placas: " .$placas . " ";
 
                         $thumbnail = "thumbnail_";
                         $img = "";
@@ -543,21 +662,24 @@ TEL. CEL. 4525257450'), 0, 'J');
 
                         $thumbnail .= $archivo;
 
-                        $result = $conn->query("SELECT id_vehiculo from vehiculos
-                            where placas = '$placas'");
-                        while($rs = $result->fetch_array(MYSQLI_ASSOC) ) {
-                            $id_vehiculo = $rs["id_vehiculo"];
+                        $id_vehiculo = getIdVe_perPlacas($placas, $conn);
+
+                        $conn->query("INSERT into fotos (placas ,ruta_img,
+                          thumbnail, id_vehiculo, id_memoria_grafica)
+                          values('$placas','$img','$thumbnail','$id_vehiculo','$id_MG')");
+
+                        // Checar si se ejecuto correctamente la sentencia, si es mayor a 0, se ejecuto correctamente
+                        if( $conn -> affected_rows > 0 )
+                        {
+                           echo "INSERT FOTOS affected_rows: " . $conn -> affected_rows ." ";
+                           //echo true;
                         }
-
-                        $conn->query("INSERT into fotos(placas, ruta_img, thumbnail, id_vehiculo,id_memoria_grafica)
-                          values ('$placas','$img','$thumbnail', '$id_vehiculo', '$id_MG')");
-                        echo true;
-
+                         else
+                           echo false;
 
                         imagedestroy($original);
                         imagedestroy($copia);
-
-                  }
+                  } // fin else ( if(file_exists) )
                 }
             } // fin if de tipo de imagenes ( gif, jpg, etc.. )
             else{
@@ -605,8 +727,7 @@ TEL. CEL. 4525257450'), 0, 'J');
       header("Access-Control-Allow-Origin: *");
       $nombreliminar = $_POST["nombreliminar"];
       $conn->query("DELETE FROM memorias_graficas
-        where placas LIKE '%$nombreliminar%'
-        || id_memoria_grafica='$nombreliminar'");
+        where id_memoria_grafica='$nombreliminar'");
 
       $conn->close();
       echo $nombreliminar;
@@ -637,7 +758,157 @@ TEL. CEL. 4525257450'), 0, 'J');
 
         break;
 
+    case 'get_img_perMg': // Metodo general, ocupa la variable 'id_MG' por POST, que sea NUMERO
+          # code...
+          $id_MG = $_POST["id_MG"];
+          $imagenes = "";
+
+          $result = $conn->query("SELECT ruta_img from fotos
+          WHERE id_memoria_grafica = $id_MG");
+
+          while($rs = $result->fetch_array(MYSQLI_ASSOC) ) {
+              $imagenes= $imagenes . $rs["ruta_img"] .",=,";
+          }
+
+          $conn->close();
+
+          //$imagenes = getImg_perMG($id_MG);
+          echo $imagenes;
+
+          break;
+
+    case 'prueba_get_datos':
+            # code...
+      $id_MG = $_POST["id_MG"];
+      //echo "id_MG: " .$id_MG;
+      $id_vehiculo = getIdVehiculo_perMG($id_MG, $conn);
+      $placas = getPlacas_perIdVe($id_vehiculo, $conn);
+      $id_cliente = getIdCliente_perIdVe($id_vehiculo, $conn);
+      $cliente = getCliente_perIdCli($id_cliente, $conn);
+
+
+      echo "id_vehiculo: " .$id_vehiculo;
+      echo "\nplacas: " .$placas;
+      echo "\nid_cliente: " .$id_cliente;
+      echo "\ncliente: " .$cliente;
+    break;
+
     default:
         echo "Conexion a BD";
 }
+
+  // Funciones SQL
+  function getCliente_perIdCli($idCli, $conn)
+  {
+    $datos_cliente="";
+    $result = $conn->query("SELECT nombre, correo, calle, colonia, ciudad, contacto1, tel_fijo
+     from clientes WHERE id_cliente = '$idCli'");
+
+          while($rs = $result->fetch_array(MYSQLI_ASSOC) ) {
+              $datos_cliente= $datos_cliente .$rs["nombre"] .",=,";
+              $datos_cliente= $datos_cliente .$rs["correo"] .",=,";
+              $datos_cliente= $datos_cliente .$rs["calle"] .",=,";
+              $datos_cliente= $datos_cliente .$rs["colonia"] .",=,";
+              $datos_cliente= $datos_cliente .$rs["ciudad"] .",=,";
+              $datos_cliente= $datos_cliente .$rs["tel_fijo"];
+          }
+
+    //$conn->close();
+
+    //echo $datos_cliente;
+    return $datos_cliente;
+  }
+
+  function getIdCliente_perIdVe($idVe, $conn)
+  {
+    $result = $conn->query("SELECT id_cliente from vehiculos
+          WHERE id_vehiculo = '$idVe'");
+
+          if($rs = $result->fetch_array(MYSQLI_ASSOC) ) {
+              $id_Cli= $rs["id_cliente"];
+          }
+
+    //$conn->close();
+
+    //echo $id_Cli;
+    return $id_Cli;
+  }
+
+  function getPlacas_perIdVe($idVe, $conn)
+  {
+    $result = $conn->query("SELECT placas from vehiculos
+          WHERE id_vehiculo = '$idVe'");
+
+          if($rs = $result->fetch_array(MYSQLI_ASSOC) ) {
+              $placas= $rs["placas"];
+          }
+
+    //$conn->close();
+
+    //echo $placas;
+    return $placas;
+  }
+
+  function getIdVehiculo_perMG($id_MG, $conn)
+  {
+    $result = $conn->query("SELECT id_vehiculo from memorias_graficas
+          WHERE id_memoria_grafica = '$id_MG'");
+
+          if($rs = $result->fetch_array(MYSQLI_ASSOC) ) {
+              $id_Ve= $rs["id_vehiculo"];
+          }
+
+    //$conn->close();
+
+    //echo $id_Ve;
+    return $id_Ve;
+  }
+
+  function getIdVe_perPlacas($placas, $conn)
+  {
+    $result = $conn->query("SELECT id_vehiculo from vehiculos
+        where placas = '$placas'");
+    while($rs = $result->fetch_array(MYSQLI_ASSOC) ) {
+        $id_vehiculo = $rs["id_vehiculo"];
+    }
+    return $id_vehiculo;
+  }
+
+  function getUltimo_idMG( $conn)
+  {
+    $result = $conn->query("SELECT id_memoria_grafica from memorias_graficas
+          ORDER BY id_memoria_grafica desc");
+
+          if($rs = $result->fetch_array(MYSQLI_ASSOC) ) {
+              $id_MG= $rs["id_memoria_grafica"];
+          }
+
+    //$conn->close();
+
+    echo $id_MG;
+    return $id_MG;
+  }
+
+  function getImg_perMG($id_MG, $conn)
+  {
+    $imagenes = "";
+    $result = $conn->query("SELECT ruta_img from fotos
+          WHERE id_memoria_grafica = $id_MG");
+
+          while($rs = $result->fetch_array(MYSQLI_ASSOC) ) {
+              $imagenes= $imagenes . $rs["ruta_img"];
+          }
+
+    //$conn->close();
+
+    //echo $imagenes;
+    return $imagenes;
+  }
+
+  // Checar si se ejecuto correctamente la sentencia, si es mayor a 0, se ejecuto correctamente
+  /*if( $conn -> affected_rows > 0 )
+     echo true;
+   else
+     echo false;*/
+
 ?>
